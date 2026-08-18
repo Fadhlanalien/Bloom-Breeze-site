83d93ef24424e03b206f3309abb75da344a332bb
Fadhlanalien <abdhulfatthahal@gmail.com>
2026-08-19 03:58:29 +0530

    add few loines for finding which database production site belongs to

--- bloom_breeze.py
+++ bloom_breeze.py
@@ -329,6 +329,7 @@ def login():
                         flash("Incorrect Password")
                 else:
                     flash("Invalid username")
+    print("hellow, this is just for test")
 
     return render_template('login.html')
 
